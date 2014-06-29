cf0873b0f4dc4a3b90a79c92eaaa9f9af2754f14
Jesper Reenberg <jesper.reenberg@gmail.com>
2014-06-29 21:41:01 +0200

    First attempt at a better @configure specification
    Implemented a 'spec' argument to @configure which will handle the uuid,
    input entity and transform set specification for each type of input you
    want to handle.
    This merges the three separate arguments to @configure which was kind of
    clumsy to define and was an easy source of error if you either of the
    three arguments didn't match in length, etc.
    
    The new 'spec' is documented in transform.plate, but the basic idea is
    to combine the arguments in a list of triples (uuid, input entity and
    list of transform sets).
    
    Backwards compatibility is maintained, by populating the 'old' fields,
    thus the original code for generating the local transforms will still
    work with this new style of specification.
    
    Also added some more fields, that will be used in the future and
    generally updated the structure and comments in the template files.

--- src/canari/framework.py
+++ src/canari/framework.py
@@ -2,6 +2,7 @@
 
 from canari.resource import external_resource
 from canari.utils.stack import calling_package
+from canari.maltego import message
 
 from subprocess import PIPE, Popen
 import os
@@ -38,23 +39,74 @@ def deprecated(f):
 
 class configure(object):
     def __init__(self, **kwargs):
-        diff = set(['label', 'uuids', 'inputs']).difference(kwargs)
-        if diff:
-            raise TypeError('Missing transform specification properties: %s' % ', '.join(diff))
-        if not isinstance(kwargs['uuids'], list):
-            raise TypeError('Expected type list (got %s instead)' % type(kwargs['uuids']).__name__)
-        if not isinstance(kwargs['inputs'], list):
-            raise TypeError('Expected type list (got %s instead)' % type(kwargs['inputs']))
+        if not kwargs.has_key('label'):
+            raise TypeError('Missing transform specification property: "label"')
+
+        if kwargs.has_key('spec'):
+            # New style specification (list of triples)
+            if not isinstance(kwargs['spec'], list):
+                raise TypeError('Expected "spec" of type list (got %s instead)' % type(kwargs['spec']).__name__)
+            if len(kwargs['spec']) < 1:
+                raise ValueError('Expected "spec" to contain atleast one element (got %s instead)' % len(kwargs['spec']))
+
+            # After verifying the types we wan't to unpack the spec list, to be
+            # backwards compatible.
+            kwargs['uuids'] = []
+            kwargs['inputs'] = []
+
+            for elem in kwargs['spec']:
+                if not len(elem) == 3:
+                    raise ValueError('Expected exactly three values for each element of the "spec" list (got %s instead)' % len(elem))
+
+                uuid, input, transform_sets = elem
+
+                if not isinstance(uuid, str):
+                    raise TypeError('Expected uuid part of the "spec" list to be of type string (got %s instead)' % type(uuid).__name__)
+                if not isinstance(input, message.MetaEntityClass):
+                    raise TypeError('Expected input part of the "spec" list to be an entity (got %s instead)' % type(input).__name__)
+                if not isinstance(transform_sets, list):
+                    raise TypeError('Expected transform sets part of the "spec" list to be of type list (got %s instead)' % type(transform_sets).__name__)
+
+                # If the user specified an empty list as the transform set, then
+                # add the empty string to the list, so we always have at least
+                # one element.
+                if not transform_sets: # Empty list
+                    transform_sets.append('')
+                for tset in transform_sets:
+                    if not isinstance(tset, str):
+                        raise TypeError('Expected elements of transform set to be of type string (got %s instead)' % type(tset).__name__)
+
+                # 'Unpack' this element, so we remain backwards compatible.
+                kwargs['uuids'].append(uuid)
+                kwargs['inputs'].append((transform_sets[0], input))
+        else:
+            # Old style specification
+            if not kwargs.has_key('uuids'):
+                raise TypeError('Missing transform specification property: "uuids"')
+            if not isinstance(kwargs['uuids'], list):
+                raise TypeError('Expected "uuids" property to be of type list (got %s instead)' % type(kwargs['uuids']).__name__)
+            if not kwargs.has_key('inputs'):
+                raise TypeError('Missing transform specification property: "inputs"')
+            if not isinstance(kwargs['inputs'], list):
+                raise TypeError('Expected "inputs" property to be of type list (got %s instead)' % type(kwargs['inputs']).__name__)
+
+        # Set optional fields
+        kwargs['owner'] = kwargs.get('owner', '')
         kwargs['description'] = kwargs.get('description', '')
+        kwargs['disclaimer'] = kwargs.get('disclaimer', '')
+        kwargs['outputs'] = kwargs.get('outputs', ['Any'])
         kwargs['debug'] = kwargs.get('debug', False)
+        kwargs['remote'] = kwargs.get('remote', False)
         self.function = kwargs.get('cmd', None)
-        self.specification = kwargs
+        # Temporarily store kwargs, so we may use them later when we are
+        # actually @decorating the function.
+        self._kwargs = kwargs
 
     def __call__(self, f):
         if callable(self.function):
             self.function.__dict__.update(f.__dict__)
             f = self.function
-        f.__dict__.update(self.specification)
+        f.__dict__.update(self._kwargs)
         return f
 
 
